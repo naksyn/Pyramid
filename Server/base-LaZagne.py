596bb86f08753a1c31b61445b6a0a0cb38098060
Sam Free5ide <snovvcrash@protonmail.ch>
2022-09-22 20:30:54 +0300

    Update header

--- Server/base-LaZagne.py
+++ Server/base-LaZagne.py
@@ -1,17 +1,25 @@
 '''
-Author: @naksyn (c) 2022
--
-Copyright 2022 naksyn
+Author: @snovvcrash (c) 2022
+
+Description: Pyramid Base script for executing LaZagne in-memory.
+
+Instructions: See README on https://github.com/naksyn/Pyramid
+
+Credits:
+  - @naksyn (Pyramid Project)
+  - Alessandro Zani (LaZagne)
+
+Copyright 2022
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-This script also contains an adaptation of https://github.com/AlessandroZ/LaZagne/blob/master/Windows/laZagne.py
+This script also contains an adaptation of LaZagne: https://github.com/AlessandroZ/LaZagne/blob/master/Windows/laZagne.py
 '''
 
 import os
